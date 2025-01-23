857036bcff9530f4630a65c99e55fb57501f53c5
goodguyjay <jair.ramos@live.com>
2025-01-23 18:17:30 -0300

    added functionality to make the core player gameplay work

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,48 @@ mod dialog;
 mod logic;
 mod ui;
 
-use card_renderer::{format_color, render_hand, Card};
-use dialog::DialogTree;
+use crate::logic::Game;
+use crate::ui::{clear_console, get_player_action, render};
 
 fn main() {
-    let card1 = Card::new("A", "Spades", format_color("Spades"));
-    let card2 = Card::new("10", "Hearts", format_color("Hearts"));
-    let card3 = Card::new("K", "Diamonds", format_color("Diamonds"));
-    let player_hand = vec![card1, card2, card3];
+    let mut game = Game::new();
+    game.deal_initial_hands();
 
-    println!("\nYour hand:");
-    println!("{}", render_hand(&player_hand));
+    loop {
+        clear_console();
+        render(&game);
+        
+        if let Some(action) = get_player_action() {
+            match action {
+                'h' => {
+                    if let Some(card) = game.deck.deal() {
+                        game.player_hand.push(card);
+                        println!("You drew a card.");
+                        
+                        let player_value = Game::calculate_hand_value(&game.player_hand);
+                        println!("Your total is: {}", player_value);
+                        
+                        if player_value > 21 {
+                            println!("You busted. Dealer wins.");
+                            break;
+                        }
+                    }
+                }
+                
+                's' => {
+                    println!("You chose to stand. Dealer's turn.");
+                    break;
+                }
+                
+                'q' => {
+                    println!("Thanks for playing.");
+                    break;
+                }
+                
+                _ => {
+                    println!("Invalid action. Please choose [H]it, [S]tand, or [Q]uit.");
+                }
+            }
+        }
+    }
 }
